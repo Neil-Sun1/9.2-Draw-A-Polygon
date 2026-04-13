b050d84cd0b299cea0bbc7f6876343a8909d5e4d
Neil <you@example.com>
2026-04-13 16:30:49 -0500

    Almost done

---
+++ main.py
@@ -0,0 +1,36 @@
+from turtle import *
+
+t= Turtle()
+s=Screen()
+s.bgcolor("black")
+t.color("white")
+
+sides = int(input("how many sides does your polygon have"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+s.mainloop()
